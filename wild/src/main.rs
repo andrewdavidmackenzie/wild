f607b6bedd6e718e18783fcfc50d7da9e6f4fd07
andrewdavidmackenzie <andrew@mackenzie-serres.net>
2024-11-22 12:39:58 +0000

    create named pipe, fork and unblock parent.

--- wild/src/main.rs
+++ wild/src/main.rs
@@ -1,7 +1,80 @@
+use anyhow::anyhow;
+use fork::{fork, Fork};
 use std::env::args;
+use std::ffi::CString;
+use std::fs::File;
+use std::io::Error;
+use std::io::{Read, Write};
 
 fn main() -> wild_lib::error::Result {
-    // Supply args to wildlib, skipping the program name
-    let linker = wild_lib::Linker::from_args(args().skip(1))?;
-    linker.run()
+    // skip the program name
+    let mut args: Vec<String> = args().skip(1).collect();
+
+    // See if there is a better way to do this, more canonical
+    let mut fork_subprocess = false;
+
+    // TODO decide if we want some short-form equivalent of "--fork"
+    args.retain(|a| {
+        if a == "--fork" {
+            fork_subprocess = true;
+            false
+        } else {
+            true
+        }
+    });
+
+    match fork_subprocess {
+        true => match make_named_pipe() {
+            Ok(path) => {
+                match fork() {
+                    Ok(Fork::Child) => {
+                        // Create a linker with remaining args and run it
+                        wild_lib::Linker::from_args(args.into_iter())?.run()?;
+
+                        // inform parent that we are done!
+                        let mut f = File::open(path)?;
+                        f.write_all("OK".as_bytes())?;
+                        Ok(())
+                    }
+                    Ok(Fork::Parent(_child)) => {
+                        // Wait for child to exit or pipe to be closed
+                        let mut f = File::open(path)?;
+                        let mut response = String::new();
+                        let child_exit_status = f.read_to_string(&mut response)?;
+                        println!("Child Exit Status: {child_exit_status}");
+                        Ok(())
+                    }
+                    Err(_) => {
+                        // Create a linker with remaining args and run it
+                        wild_lib::Linker::from_args(args.into_iter())?.run()
+                    }
+                }
+            }
+            Err(_) => {
+                // Create a linker with remaining args and run it
+                wild_lib::Linker::from_args(args.into_iter())?.run()
+            }
+        },
+        false => {
+            // Create a linker with remaining args and run it
+            wild_lib::Linker::from_args(args.into_iter())?.run()
+        }
+    }
+}
+
+/// Create a named pipe for communication between parent and child processes.
+/// If successful it will return Ok with the name of the file
+/// If errors it will return an error message with the errno set, if it can be read or -1 if not
+fn make_named_pipe() -> wild_lib::error::Result<String> {
+    let path = "named_pipe";
+    let filename = CString::new(path)?;
+    unsafe {
+        match libc::mkfifo(filename.as_ptr(), 0o644) {
+            0 => Ok(path.to_owned()),
+            _ => Err(anyhow!(
+                "Error creating named pipe. Errno = {:?}",
+                Error::last_os_error().raw_os_error().unwrap_or(-1)
+            )),
+        }
+    }
 }
